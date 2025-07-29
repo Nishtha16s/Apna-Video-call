ed62bdb99ff872a2c0ec4a5dacc8a39154d46f94
Nishtha Roy <nishtharoy1234@gmail.com>
2025-07-29 15:42:42 +0530

    DEPLOYING FRONTEND

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,8 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+ "https://apnavideocall-backend-mvlk.onrender.com" :
 
-    "http://localhost:8000"
+ "http://localhost:8000"
 
 
 export default server;
